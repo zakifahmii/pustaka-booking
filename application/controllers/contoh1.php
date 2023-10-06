c8100e82be2702b9e8f772aa33c6b8b1f9f28da2
krisna-1 <krisnaaditama34@gmail.com>
2023-10-06 18:56:32 +0700

    tugas pertemuan 2 contoh1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan </h1>";
+        echo "Nama Saya Krisna Aditama
+            Saya Tinggal di jakarta
+            Olahraga yang Saya Sukai Adalah
+            Sepak Bola";    
+    }
+}
